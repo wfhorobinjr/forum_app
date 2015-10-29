30d423256cf356b4ec25e21d82a4eb11ec6bbff9
Bill <wfhorobinjr@quinnipiac.edu>
2015-10-29 23:53:22 +0000

    Add thread list view

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
   url(r'^user/', include('registration.backends.simple.urls')),
   url(r'^user/', include('django.contrib.auth.urls')),
   url(r'^thread/create/$', ThreadCreateView.as_view(), name='thread_create'),
+  url(r'^thread/$', ThreadListView.as_view(), name='thread_list'),
 )
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from django.core.urlresolvers import reverse_lazy
 from .models import*
 
@@ -12,8 +12,12 @@ class ThreadCreateView(CreateView):
   model = Thread
   template_name = "thread/thread_form.html"
   fields = ['title', 'link', 'text']
-  success_url = reverse_lazy('home')
+  success_url = reverse_lazy('thread_list')
 
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super(ThreadCreateView, self).form_valid(form)
+
+class ThreadListView(ListView):
+  model = Thread
+  template_name = "thread/thread_list.html"

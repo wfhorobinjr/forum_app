7e4e2d01e328a5069dec20eec6384da12d6a3850
Bill <wfhorobinjr@quinnipiac.edu>
2015-10-29 23:14:34 +0000

    Add user Authentication

--- forum_app/settings.py
+++ forum_app/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'registration',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,3 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),)
 STATIC_ROOT = 'staticfiles'
+
+LOGIN_URL = '/user/login'
+LOGIN_REDIRECT_URL = '/'
+SITE_ID = 1
